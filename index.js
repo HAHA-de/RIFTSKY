02b0cb22f382acb564346795d52215c099bdd970
ru23go <yowest11@hotmail.com>
2023-02-01 07:50:36 -0500

    hhhhh

--- index.js
+++ index.js
@@ -1,17 +1,1 @@
-var articlbtn = document.getElementsByClassName("articlebtn");
-var no = document.getElementsByClassName("no");
-function thanks() {var thx  = prompt("wecome (say thank you)")}
-if(thx == "thank you"){
-
-    alert("ur wecome");
-}
-
-else if(thx == "thx"){
-
-    alert("ur wecome");
-}
-else{
-
-    alert("rude of you not to say thx or thank you");
-    thanks()
-}
+alert("java script loaded")
