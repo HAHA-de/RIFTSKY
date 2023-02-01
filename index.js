8d31cc2d13d899d1a147b15b6412f9d9e3bcbc98
ru23go <yowest11@hotmail.com>
2023-02-01 07:48:07 -0500

    h

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
-var thx  = prompt("wecome (say thank you)")
-var articlbtn = document.getElementsByClassName("articlebtn")
-var no = document.getElementsByClassName("no")
-
+var articlbtn = document.getElementsByClassName("articlebtn");
+var no = document.getElementsByClassName("no");
+function thanks() {var thx  = prompt("wecome (say thank you)")}
 if(thx == "thank you"){
 
-    alert("ur wecome")
+    alert("ur wecome");
 }
 
 else if(thx == "thx"){
 
-    alert("ur wecome")
+    alert("ur wecome");
 }
 else{
 
-    alert("rude of you not to say thx or thank you")
+    alert("rude of you not to say thx or thank you");
+    thanks()
 }
