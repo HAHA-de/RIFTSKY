69c57d2affa101d16408a6e6acca087328ef1dd5
ru23go <yowest11@hotmail.com>
2023-02-01 07:44:58 -0500

    added java script

---
+++ index.js
@@ -0,0 +1,17 @@
+var thx  = prompt("wecome (say thank you)")
+var articlbtn = document.getElementsByClassName("articlebtn")
+var no = document.getElementsByClassName("no")
+
+if(thx == "thank you"){
+
+    alert("ur wecome")
+}
+
+else if(thx == "thx"){
+
+    alert("ur wecome")
+}
+else{
+
+    alert("rude of you not to say thx or thank you")
+}
